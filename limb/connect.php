2704d6cdf456dbeb1485d68f193c0fbe790cf1c7
Gennadii <povgen@yandex.ru>
2023-01-14 14:40:20 +0300

    Настроил подключение

--- limb/connect.php
+++ limb/connect.php
@@ -1,7 +1,7 @@
 <?php 
 
 require 'rb.php';
-R::setup( 'mysql:host=127.0.0.1;dbname=factorio-draft','root','' );
+R::setup( 'mysql:host=127.0.0.1;dbname=factorio','root','root' );
 session_start();
 
 
